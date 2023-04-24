f79e1040d737b4e834f5effe88f46d8fdf9e9558
joe1-code <john4m2@gmail.com>
2023-04-24 14:19:26 +0300

    finished axiosConfigFile

---
+++ src/Config/configFile.js
@@ -0,0 +1,90 @@
+"use strict";
+
+import axios from 'axios';
+
+let instance;
+let BASE_URL;
+let subscriber = [];
+let isAlreadyFetchingAccessToken = false;
+
+const token = localStorage.getItem('token');
+
+BASE_URL = process.env.REACT_APP_VICOBA_API_URL;
+
+const defaultOptions = {
+ baseURL: BASE_URL,
+ withCredentials: true,
+ headers: {
+  'Content-Type': 'application/json',
+ },
+};
+
+if (!instance) {
+ instance = axios.create(defaultOptions)
+ instance.interceptors.request.use(function (config) {
+  if (token) {
+   config.headers.Authorization = token ? `Bearer ${token}` : ' ';
+  }
+  return config;
+ });
+
+ function onAccessTokenFetched(access_token) {
+  subscriber = subscriber.filter((callback) => callback(access_token));
+ }
+
+ function addSubscriber(callback) {
+  subscriber.push(callback);
+ }
+
+ instance.interceptors.response.use(
+
+  function (response) {
+   return response.data;
+  },
+
+  function (error) {
+   const {
+    config,
+    response: { status }
+   } = error;
+
+   let originalRequest = config;
+
+   if (originalRequest.url === '/link') {
+    return;
+   }
+   else {
+    if (status === 405) {
+     if (!isAlreadyFetchingAccessToken) {
+      isAlreadyFetchingAccessToken = true;
+
+      fetchAccessToken().then((access_token) => {
+       if (access_token) {
+        const { AccessToken } = access_token.data;
+        isAlreadyFetchingAccessToken = false;
+        onAccessTokenFetched(AccessToken);
+       }
+      });
+
+      const retryOriginalRequest = new Promise((resolve) => {
+       addSubscriber((AccessToken) => {
+        const newToken = 'Bearer' + AccessToken;
+        const newheaders = {
+         ...originalRequest.headers,
+         Authorization: newToken
+        };
+
+        const newrequest = { ...originalRequest, headers: newheaders };
+        const newinstance = axios.create();
+        resolve(newinstance(newrequest));
+       });
+      });
+
+      return retryOriginalRequest;
+     }
+    }
+   }
+  }
+
+ )
+}
