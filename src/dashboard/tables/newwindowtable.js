881d2065fddaac68982e2474fa3c8db156a3732c
joe1-code <john4m2@gmail.com>
2023-05-19 14:01:30 +0300

    display users from db successfully

--- src/dashboard/tables/newwindowtable.js
+++ src/dashboard/tables/newwindowtable.js
@@ -12,133 +12,122 @@ import { getUsers } from '../../client/client';
 import { useEffect } from 'react';
 import { useState } from 'react';
 
-
-
 const checks = (<Checkboxes />);
-
-
-const sample = [
-  ['Joel John', checks],
-  ['Hamis Hakim', checks],
-  ['Abdul Ndugu', checks],
-  ['Jumbe Anna', checks],
-  ['Paul Chula', checks],
-  ['Khalid Amed', checks],
-  ['Koku Banza', checks],
-  ['Figo Luis', checks],
-  ['Ally Mjahidin', checks]
-
-];
-
-
-function createData(id, names, participate) {
-  return { id, names, participate };
-}
-
-const columns = [
-  {
-    width: 10,
-    label: 'S/N',
-    dataKey: 'id',
-    numeric: true
-  },
-  {
-    width: 10,
-    label: 'Names',
-    dataKey: 'names',
-  },
-  {
-    width: 10,
-    label: 'Participate',
-    dataKey: 'participate',
-  }
-];
-
-const rows = Array.from({ length: 50 }, (_, index) => {
-  const randomSelection = sample[Math.floor(Math.random() * sample.length)];
-  return createData(index, ...randomSelection);
-});
-
-const VirtuosoTableComponents = {
-  Scroller: React.forwardRef((props, ref) => (
-    <TableContainer component={Paper} {...props} ref={ref} />
-  )),
-  Table: (props) => (
-    <Table {...props} sx={{ borderCollapse: 'separate', tableLayout: 'fixed' }} />
-  ),
-  TableHead,
-  TableRow: ({ item: _item, ...props }) => <TableRow {...props} />,
-  TableBody: React.forwardRef((props, ref) => <TableBody {...props} ref={ref} />),
-};
-
-function fixedHeaderContent() {
-  return (
-    <TableRow>
-      {columns.map((column) => (
-
-        <TableCell
-          key={column.dataKey}
-          variant="head"
-          align={column.numeric || false ? 'right' : 'left'}
-          style={{ width: column.width }}
-          sx={{
-            backgroundColor: 'background.paper',
-          }}
-        >
-          {column.label}
-        </TableCell>
-      ))}
-    </TableRow>
-  );
-}
-
-function rowContent(_index, row) {
-
-  return (
-    <React.Fragment>
-      {columns.map((column) => (
-        <TableCell
-          key={column.dataKey}
-          align={column.numeric || false ? 'right' : 'left'}
-        >
-          {row[column.dataKey]}
-        </TableCell>
-      ))}
-    </React.Fragment>
-  );
-}
 
 function NewwindowTable() {
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
     fetchdata();
-  }, [])
+  }, []);
 
   async function fetchdata() {
-    const response = await getUsers();
-    if (response) {
-      console.log(response.data);
+    const data = await getUsers();
+    if (data) {
 
-      // const sample = [
-      //   ['Joel John', checks],
-      //   ['Hamis Hakim', checks],
-      //   ['Abdul Ndugu', checks],
-      //   ['Jumbe Anna', checks],
-      //   ['Paul Chula', checks],
-      //   ['Khalid Amed', checks],
-      //   ['Koku Banza', checks],
-      //   ['Figo Luis', checks],
-      //   ['Ally Mjahidin', checks]
+      const members = data.data.map((item) => {
+        // const { fullname } = item.fullname;
+        return [item.fullname, checks];
+      })
+      console.log(members)
 
-      // ];
-      setUsers(sample)
+      setUsers(members)
     }
     else {
       console.log("no data")
     }
 
   }
+
+  // ---------------------------------------------------------------------
+
+
+  function createData(id, names, participate) {
+    return { id, names, participate };
+  }
+
+  const columns = [
+    {
+      width: 10,
+      label: 'S/N',
+      dataKey: 'id',
+      numeric: true
+    },
+    {
+      width: 10,
+      label: 'Names',
+      dataKey: 'names',
+    },
+    {
+      width: 10,
+      label: 'Participate',
+      dataKey: 'participate',
+    }
+  ];
+
+  // const rows = Array.from({ length: 5 }, (_, index) => {
+  //   const randomSelection = users[Math.floor(Math.random() * users.length)];
+  //   return createData(index, ...randomSelection);
+  // });
+
+  const rows = [];
+  for (let i = 0; i < users.length; i++) {
+    const userData = users[i]
+    const rowData = createData(i, ...userData)
+    rows.push(rowData)
+  }
+
+  const VirtuosoTableComponents = {
+    Scroller: React.forwardRef((props, ref) => (
+      <TableContainer component={Paper} {...props} ref={ref} />
+    )),
+    Table: (props) => (
+      <Table {...props} sx={{ borderCollapse: 'separate', tableLayout: 'fixed' }} />
+    ),
+    TableHead,
+    TableRow: ({ item: _item, ...props }) => <TableRow {...props} />,
+    TableBody: React.forwardRef((props, ref) => <TableBody {...props} ref={ref} />),
+  };
+
+  function fixedHeaderContent() {
+    return (
+      <TableRow>
+        {columns.map((column) => (
+
+          <TableCell
+            key={column.dataKey}
+            variant="head"
+            align={column.numeric || false ? 'right' : 'left'}
+            style={{ width: column.width }}
+            sx={{
+              backgroundColor: 'background.paper',
+            }}
+          >
+            {column.label}
+          </TableCell>
+        ))}
+      </TableRow>
+    );
+  }
+
+  function rowContent(_index, row) {
+
+    return (
+      <React.Fragment>
+        {columns.map((column) => (
+          <TableCell
+            key={column.dataKey}
+            align={column.numeric || false ? 'right' : 'left'}
+          >
+            {row[column.dataKey]}
+          </TableCell>
+        ))}
+      </React.Fragment>
+    );
+  }
+
+  // ----------------------------------------------------------------------
+
 
 
   return (
