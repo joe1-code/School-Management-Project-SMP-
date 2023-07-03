e89cc357c377fca4962c1c3d182367af3cd20506
joe1-code <john4m2@gmail.com>
2023-07-03 18:06:35 +0300

    solved the date issue during rendering of view window

--- src/dashboard/tables/windowTable.js
+++ src/dashboard/tables/windowTable.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import TableContainer from "@mui/material/TableContainer";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
@@ -13,22 +13,34 @@ import { getUsers, windowUsers } from '../../client/client';
 
 function WindowDataTable() {
 
+  const [startdate, setStartdate] = useState('');
+  const [receivers, setReceivers] = useState('');
+  const [results, setResults] = useState('');
+
   useEffect(() => {
     windowShow();
   }, []);
 
+
   async function windowShow() {
     try {
       const response = await windowUsers();
-
+      console.log("star", response)
       // destructuring startdate value from a response object
       const startdate = response.data[0].startdate;
       const receivers = response.data[0].receivers;
-      console.log("this is receivers", receivers)
-      console.log('this is startdate', startdate);
+      // const { startdate, receivers } = response.data[0].
 
-      const enddateResults = enddates(startdate, receivers);
-      console.log("this is end date:", enddateResults);
+      //update the state using the received values
+      setStartdate(startdate);
+      setReceivers(receivers);
+
+      // console.log("this is receivers", receivers)
+      // console.log('this is startdate', startdate);
+
+      const results = enddates(startdate, receivers);
+
+      setResults(results);
 
 
     }
@@ -40,17 +52,14 @@ function WindowDataTable() {
   }
 
 
-
   //  calculate for an endcycle logic
   function enddates(startdate, receivers) {
-    console.log("testssssssssss")
+
     //parse the startdate
     const startdateObj = new Date(startdate);
 
     //calculate the enddate after a day
     const enddateDay = new Date(startdateObj.getTime() + receivers * 24 * 60 * 60 * 1000);
-    console.log('this is enddateday', enddateDay);
-
 
     // calculate the enddate after a week
     const enddateWeek = new Date(startdateObj.getTime() + (receivers * 7) * 24 * 60 * 60 * 1000);
@@ -58,14 +67,17 @@ function WindowDataTable() {
     // calculate the enddate after a month
     const enddateMonth = new Date(startdateObj.getTime() + (receivers * 30) * 24 * 60 * 60 * 1000);
 
-    return {
-      enddateDay: enddateDay,
-      enddateWeek: enddateWeek,
-      enddateMonth: enddateMonth
+    const duration = receivers * 24 * 60 * 60 * 1000;
+
+    if (duration <= enddateDay - startdateObj) {
+      return { label: "day", value: enddateDay.toISOString() };
     }
-
-
-
+    else if (duration <= enddateWeek - startdateObj) {
+      return { label: "week:", value: enddateWeek.toISOString() };
+    }
+    else {
+      return { label: "month", value: enddateMonth.toISOString() };
+    }
 
   }
 
@@ -75,17 +87,24 @@ function WindowDataTable() {
 
 
   function WindowData(id, startcycle, endcycle, actions) {
-    return { id, startcycle, endcycle, actions };
+    return { id, startcycle, endcycle: endcycle.value, actions };
   }
 
-  const windowActions = (<div><VisibilityIcon /> <CreateIcon /></div>)
+
+  const windowActions = (<div><VisibilityIcon /> <CreateIcon /></div>);
+
+  console.log("sttatatata", startdate)
+
+  // if (!(startdate instanceof Date)) {
+  //   console.log("startdate is not a Date object.");
+  //   return <div>Error: Invalid start date.</div>; // or render an error message
+  // }
+  // const formattedStartdate = startdate.toLocaleDateString();
+
   const rows = [
-    WindowData(1, 'Feb 2 2021', 'Feb 2 2022', windowActions)
+
+    WindowData(1, startdate, results, windowActions)
   ];
-
-
-
-
 
 
 
@@ -107,8 +126,8 @@ function WindowDataTable() {
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
               <TableCell align='right'>{row.id}</TableCell>
-              <TableCell component="th" scope="row">{row.startcycle}</TableCell>
-              <TableCell align="right">{row.endcycle}</TableCell>
+              <TableCell component="th" scope="row">{row.startcycle ? row.startcycle.value : 'N/A'}</TableCell>
+              <TableCell align="right">{row.endcycle ? row.endcycle : 'N/A'}</TableCell>
               <TableCell align="right">{row.actions}</TableCell>
 
 
