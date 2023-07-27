efaaedbca2f5b26a1e4da4ef6a5ab1d7a3622b06
joe1-code <john4m2@gmail.com>
2023-07-27 08:26:06 -0400

    finished classes component

--- src/Dashboard/class_components/classes.js
+++ src/Dashboard/class_components/classes.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Card } from "@mui/material";
 import TextField from '@mui/material/TextField';
 import Link from "@mui/material/Link";
+import SchoolIcon from '@mui/icons-material/School';
+import pic from "./class.png";
 import "./classes.css";
 
 function Classes(){
@@ -23,20 +25,25 @@ function Classes(){
             "title":"registered class",
             "classform":"form Four"
         }
+        
+
+        
     ]
 
 
     return(
         <div id="class-container">
             <h3 id="title">Registered Class</h3>
-            <div id="card-div"> 
+         <div id="card-div"> 
             {cardData.map((card, index)=>(
                 
                 <Card id="class-card" key={index}>
+                    {/* <img src={pic} id="avt"/> */}
+                 <SchoolIcon/>
                  <span>{card.classform}</span>
                 </Card>
             ))}
-            </div>
+         </div>
            
         </div>
     )
